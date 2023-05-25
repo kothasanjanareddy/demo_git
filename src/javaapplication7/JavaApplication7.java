cd9229e98f007184ce0c0c0d94304d7f5c780ef0
kothasanjanareddy <133983676+kothasanjanareddy@users.noreply.github.com>
2023-05-25 11:13:37 -0400

    Update JavaApplication7.java
    first change

--- src/javaapplication7/JavaApplication7.java
+++ src/javaapplication7/JavaApplication7.java
@@ -15,7 +15,7 @@ public class JavaApplication7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int b = 12;// TODO code application logic here
     }
 
 }
